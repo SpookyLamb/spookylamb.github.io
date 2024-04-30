8e379a6ac4d8a24673796352062fcb487eb4d2b7
SpookyLamb <turtleytyrant@gmail.com>
2024-04-30 16:14:52 -0400

    New designs.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import Navbar from 'react-bootstrap/Navbar'
 
 import { Link } from "react-router-dom"
 
@@ -20,13 +21,13 @@ function Hero() {
               </div>
           </div>
           <div className="row text-white text-center">
-              <h3 className="font-monospace">&#123; I'm a full-stack Web Developer! &#125;</h3>
-              <h6>(And a very spooky game developer, too!)</h6>
+              <h2 className="font-monospace">&#123; I'm a very spooky Web Developer! &#125;</h2>
+              {/* <h6>(And a very spooky game developer, too!)</h6> */}
           </div>
           <div className="row text-white text-center">
-              <h3 className="font-monospace">&#123; Wanna see what I can do? &#125;</h3>
-              <h4>(Then scroll down!)</h4>
-              <h6>(If you dare!)</h6>
+              <h3 className="font-monospace">&#123; Scroll down to see what I can do! &#125;</h3>
+              {/* <h4>(Then scroll down!)</h4> */}
+              {/* <h6>(If you dare!)</h6> */}
           </div>
       </div>
     </div>
@@ -35,36 +36,42 @@ function Hero() {
 
 function Portfolio() {
   return (
-    <Container>
+    <Container className="pt-5">
       <Row className="d-flex justify-content-center">
-        <PortfolioCard
-          title="Mind Reader"
-          pic="https://raw.githubusercontent.com/SpookyLamb/spookylamb.github.io/main/img/avi.png"
-          text="A simple app that takes any number between 1-99, and will READ YOUR MIND!"
-          link="https://spookylamb.github.io/mind-reader/"
-          btn="Clairvoyance!"
-        />
-        <PortfolioCard
-          title="Weather Report"
-          pic="https://raw.githubusercontent.com/SpookyLamb/spookylamb.github.io/main/img/avi.png"
-          text="An app that pulls up the current weather in your area, or in a provided ZIP code!"
-          link="https://spookylamb.github.io/weather-app/"
-          btn="Control Weather!"
-        />
-        <PortfolioCard 
-          title="Tic-Tac-Toe & Connect 4"
-          pic="https://raw.githubusercontent.com/SpookyLamb/spookylamb.github.io/main/img/avi.png"
-          text="An app that lets you play Tic-Tac-Toe and Connect 4! Try to beat your friends!"
-          link="https://spookylamb.github.io/tic-tac-toe/"
-          btn="Game On!"
-        />
-        <PortfolioCard 
-          title="The Summer Demon"
-          pic="https://raw.githubusercontent.com/SpookyLamb/spookylamb.github.io/main/img/avi.png"
-          text="A menu for a (fake) confectionary restaurant. Peruse our diabolical treats!"
-          link="https://spookylamb.github.io/random-restaurant/"
-          btn="Enjoy Our Menu..."
-        />
+        <Col className="col-12 col-lg-4">
+            <PortfolioCard
+              title="Mind Reader"
+              pic="https://raw.githubusercontent.com/SpookyLamb/spookylamb.github.io/main/img/mind-reader.png"
+              text="A simple app that takes any number between 1-99, and will READ YOUR MIND!"
+              link="https://spookylamb.github.io/mind-reader/"
+              btn="Clairvoyance!"
+            />
+        </Col>
+        <Col className="col-12 col-lg-4">
+          <PortfolioCard
+            title="Weather Report"
+            pic="https://raw.githubusercontent.com/SpookyLamb/spookylamb.github.io/main/img/weather-app.png"
+            text="An app that pulls up the current weather in your area, or in a provided ZIP code!"
+            link="https://spookylamb.github.io/weather-app/"
+            btn="Control Weather!"
+          />
+          <PortfolioCard 
+            title="Tic-Tac-Toe & Connect 4"
+            pic="https://raw.githubusercontent.com/SpookyLamb/spookylamb.github.io/main/img/connect-4.png"
+            text="An app that lets you play Tic-Tac-Toe and Connect 4! Try to beat your friends!"
+            link="https://spookylamb.github.io/tic-tac-toe/"
+            btn="Game On!"
+          />
+        </Col>
+        <Col className="col-12 col-lg-4">
+          <PortfolioCard 
+            title="The Summer Demon"
+            pic="https://raw.githubusercontent.com/SpookyLamb/spookylamb.github.io/main/img/summer-demon.png"
+            text="A menu for a (fake) confectionary restaurant. Peruse our diabolical treats!"
+            link="https://spookylamb.github.io/random-restaurant/"
+            btn="Enjoy Our Menu..."
+          />
+        </Col>
       </Row>
     </Container>
   )
@@ -78,7 +85,7 @@ function PortfolioCard(props) {
   const card_title = props.title
 
   return (
-    <Col className="col-12 col-lg-4 px-1 py-3">
+    <Col className="col-12 px-1 py-3">
       <Card className="portfolio-card border mx-auto" bg="dark" text="light">
         <Card.Img variant="top" src={pic_href}></Card.Img>
         <Card.Body>
@@ -92,10 +99,10 @@ function PortfolioCard(props) {
 }
 
 function InfernoDivider() {
-  return (
-    <div>
+  //a "navbar" that we're using as a divider
 
-    </div>
+  return (
+    <Navbar className="inferno-divider bg-danger border-top border-bottom"></Navbar>
   )
 }
 
@@ -107,6 +114,7 @@ function App() {
         <Hero/>
       </section>
       <section id="projects">
+        <InfernoDivider/>
         <Portfolio/>
       </section>
     </div>
